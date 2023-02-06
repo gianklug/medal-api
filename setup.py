de6a7d31627fc439d1c10b081a7baf1a29bc1ed5
Gian Klug <gian.klug@ict-scouts.ch>
2023-02-06 19:46:01 +0100

    feat: add code for v0.1.4

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-BUILD_ID = os.environ.get("BUILD_BUILDID", "3")
+BUILD_ID = os.environ.get("BUILD_BUILDID", "4")
+
+with open("requirements.txt", "r") as f:
+    packages = f.readlines()
 
 setup(
     name="medal-api",
@@ -13,9 +16,8 @@ setup(
     author="Gian Klug",
     author_email="gian.klug@ict-scouts.ch",
     url="https://github.com/gianklug/medal-api",
-    packages=find_packages("medal_api"),
-    package_dir={"": "medal_api"},
-    setup_requires=["requests"],
+    packages=["medal_api"],
+    setup_requires=packages,
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
